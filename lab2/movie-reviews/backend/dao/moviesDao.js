acc65b69cb954a525e3083552fd6f24c8979bf02
phuong732 <154291682+phuong732@users.noreply.github.com>
2026-03-24 10:28:06 +0700

    Update moviesDao.js

--- lab2/movie-reviews/backend/dao/moviesDao.js
+++ lab2/movie-reviews/backend/dao/moviesDao.js
@@ -1,1 +1,30 @@
+let movies;
 
+export default class MoviesDAO {
+  static async injectDB(conn) {
+    if (movies) return;
+    try {
+      movies = await conn.db(process.env.MOVIEREVIEWS_NS).collection('movies');
+    } catch (e) {
+      console.error(`Unable to connect in MoviesDAO: ${e}`);
+    }
+  }
+
+  static async getMovies(filters = {}, page = 0, moviesPerPage = 20) {
+    let query = {};
+    if (filters.title) {
+      query.title = { $regex: filters.title, $options: 'i' };
+    }
+
+    let cursor;
+    try {
+      cursor = await movies.find(query).limit(moviesPerPage).skip(moviesPerPage * page);
+      const moviesList = await cursor.toArray();
+      const totalNumMovies = await movies.countDocuments(query);
+      return { moviesList, totalNumMovies };
+    } catch (e) {
+      console.error(`Unable to get movies: ${e}`);
+      return { moviesList: [], totalNumMovies: 0 };
+    }
+  }
+}
