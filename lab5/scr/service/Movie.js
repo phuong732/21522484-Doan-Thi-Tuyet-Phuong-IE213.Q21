8bdd2114073e8c8c01e37c25ae9d633c75ad67eb
phuong732 <154291682+phuong732@users.noreply.github.com>
2026-05-05 15:23:33 +0700

    Update Movie.js

--- lab5/scr/service/Movie.js
+++ lab5/scr/service/Movie.js
@@ -1,1 +1,44 @@
+import axios from "axios";
 
+const BASE_URL = 'http://localhost:80/api/v1';
+
+class MovieDataService {
+  // Lấy danh sách phim có phân trang
+  getAll(page = 0) {
+    return axios.get(`${BASE_URL}/movies?page=${page}`);
+  }
+
+  // Lấy chi tiết phim theo ID
+  get(id) {
+    return axios.get(`${BASE_URL}/movies/id/${id}`);
+  }
+
+  // Tìm kiếm phim theo title hoặc rated
+  find(query, by = "title", page = 0) {
+    return axios.get(`${BASE_URL}/movies?${by}=${query}&page=${page}`);
+  }
+
+  // Tạo review mới
+  createReview(data) {
+    return axios.post(`${BASE_URL}/movies/review`, data);
+  }
+
+  // Cập nhật review
+  updateReview(data) {
+    return axios.put(`${BASE_URL}/movies/review`, data);
+  }
+
+  // Xóa review
+  deleteReview(id, userId) {
+    return axios.delete(`${BASE_URL}/movies/review`, {
+      data: { review_id: id, userId: userId }
+    });
+  }
+
+  // Lấy danh sách ratings
+  getRatings() {
+    return axios.get(`${BASE_URL}/movies/ratings`);
+  }
+}
+
+export default new MovieDataService();
